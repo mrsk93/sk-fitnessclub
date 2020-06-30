e0c29e783714945f86f6475aca7fc5e30c38f13d
Sumit Chalotra <kumar.sumit9981@gmail.com>
2020-06-30 15:16:37 +0530

    sendgrid mail service added in api

---
+++ backend/middlewares/send-query-mails.js
@@ -0,0 +1,33 @@
+const sgMail = require('@sendgrid/mail');
+
+module.exports = (req,res,next) => {
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msgToUser = {
+    to: req.body.emailId,
+    from: 'kumar.sumit9981@gmail.com',
+    subject: 'Hello There ' + req.body.name + ' -SK Fitness Club' ,
+    text: 'Thanks for checking out SK Fitness Club',
+    html: '<strong>Thanks for checking out SK Fitness Club. We got your query and we will get back to you soon.</strong>',
+  };
+  sgMail.send(msgToUser)
+  .catch(error => {
+    console.log(error.response.body);
+  });
+
+  const msgToAdmin = {
+    to: 'sumitletsrock@gmail.com',
+    from: 'kumar.sumit9981@gmail.com',
+    subject: 'You Got a New Query at SK Fitness Club',
+    text: 'Query from ' + req.body.name,
+    html: '<strong>' + req.body.description + '</strong>',
+  };
+  sgMail.send(msgToAdmin)
+  .catch(error => {
+    console.log(error.response.body);
+  });
+
+  next();
+
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const UserController = require("../controllers/user");
 
+const sendQueryMails = require("../middlewares/send-query-mails");
+
 router.get('/getSlides',UserController.getSlides);
 
 router.get('/getFeeds',UserController.getFeeds);
 
-router.post('/createQuery',UserController.createQuery);
+router.post('/createQuery', sendQueryMails, UserController.createQuery);
 
 module.exports = router;
